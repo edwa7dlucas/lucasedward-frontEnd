ea7169e606a8a400d512a31754c909bb80d1bc99
edwa7dlucas <144730110+edwa7dlucas@users.noreply.github.com>
2023-09-11 16:58:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-let insira = prompt("Insira um número par.")
-let insira2 = Number(insira)
-let resultado = insira2 % 2
-console.log(resultado)
+
+
+
+</body>
+</html>
+
+
+
+<p>você pode
+    escrever um
+    parágrafo de 
+    qualquer jeito.
+    basta colocar 
+    tudo no meio do 
+    par de tags &lt;p&gt; e &lt;/p&gt;
+</p>
+
+<p>se precisar quebrar 
+o texto em algum lugar
+especifico <br> como esse, 
+você pode usar a tag &lt;br&gt;
+</p>
+
+<h1>exercicío 1 </h1>
+ <body>
+    <h2>Na luz da verdade</h1>
+    <p> com o nome de batismo de Oskar Ernst Bernhardt, era alemão e nasceu em 18 de abril de 1875 em Bischofswerda (perto de Dresden). O nome de Abd-ru-shin, sob o qual escreveu as dissertações para a sua “Mensagem do Graal”, corresponde ao sentido de “servo da Luz”.</p>
+
+    <h2>Na luz da verdade</h1>
+    <p> com o nome de batismo de Oskar Ernst Bernhardt, <br>
+        era alemão e nasceu em 18 de abril de 1875 em Bischofswerda <br>
+        (perto de Dresden). O nome de Abd-ru-shin, sob o qual escreveu as dissertações
+        <br> para a sua “Mensagem do Graal”, corresponde ao sentido de “servo da Luz”.</p>
+</body>
+
+
+<body>
+    <h1> exercicio 2</h1>
+    
+    <p>	<h2>&copy; </h2> (a) serve para ver se o site<br> tem direitos autorais.</p>
+    <p> <h2>&euro;</h2>  (b) mostra o simbulo do euro.</p>
+    <p>  <h2>&reg;</h2>   (c) mostra que a marca do site<br>esta registrada.</p>
+    <p>  <h2> &cent;</h2>  (d) mostra o simbulo do sentavo.</p>
+    <p>  <h2>&pound;</h2> (e) mostra o simbulo de libras.<p>
+    
+</body>
+
+&#x1F603; 😃
+&#x1F615; 😕
+&#x2615;  ☕
+&#x1F3FB; 👍🏻
+
+
+
+<body>
+    <p> ao acordar de manhã eu tomo (café)&#x2615;<br>
+     quando chega sexta eu fico (feliz)&#x1F603;<br>
+     quando recebi aquela noticía fiquei (desepcionado)&#x1F615;<br>
+     gosto de comer  &#xFE0F; as vezes (hamburguer)</p><br>
+</body>
 
  
